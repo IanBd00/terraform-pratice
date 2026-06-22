6e23508e57f4da72e3dabe026d469c235d261225
Ian Luiz de Souza Bello <ian.bello@globalsys.com.br>
2026-06-22 12:50:42 -0300

    delete generated.tf

--- generated.tf
+++
@@ -1,4 +0,0 @@
-# __generated__ by Terraform
-# Please review these resources and move them into your main configuration files.
-
-# __generated__ by Terraform from "ocid1.subnet.oc1.sa-saopaulo-1.aaaaaaaafrspkyxwkuyswdcobxzhfnlnmof5zlejebd5cn7xezpu4lgdbgtq"
